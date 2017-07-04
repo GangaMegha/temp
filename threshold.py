82c38d7c74f4980e21fae2532010f2bb203b0d3c
GangaMegha <gangamegha29@google.com>
2017-07-04 16:26:43 +0530

    new message

--- threshold.py
+++ threshold.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 print "namaste ji"
+
 img = cv2.imread('ball_15cm.jpg',0)
 ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 # ret,thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
